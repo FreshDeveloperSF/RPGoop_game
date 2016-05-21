7768475bf5dcb2be9fb4daa1d457bcac13e6dd85
Jason Leung <jasonleung@jasons-mbp-3>
2016-05-21 11:16:47 -0700

    Created Kimora class and overrides super class function

---
+++ rpgoop/DevilWizard.swift
@@ -0,0 +1,15 @@
+//
+//  DevilWizard.swift
+//  rpgoop
+//
+//  Created by Jason Leung on 5/21/16.
+//  Copyright © 2016 Jason Leung. All rights reserved.
+//
+
+import Foundation
+
+class DevilWizard: Enemy {
+    override var loot: [String] {
+        return ["Magic Wand", "Dark Amulet", "Salted Pork"]
+    }
+}
