1b414c61df89d41c3ec4aa32ded56ff0c362b776
Jason Leung <jasonleung@jasons-mbp-3>
2016-05-21 11:06:54 -0700

    Created Character, Player and Enemy classes

---
+++ rpgoop/Kimara.swift
@@ -0,0 +1,16 @@
+//
+//  Kimara.swift
+//  rpgoop
+//
+//  Created by Jason Leung on 5/21/16.
+//  Copyright © 2016 Jason Leung. All rights reserved.
+//
+
+import Foundation
+
+class Kimara: Enemy {
+    
+    let IMMUNE_MAX = 15
+    
+    
+}
